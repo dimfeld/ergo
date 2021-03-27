77b222a3a626dbbe7d3ac046987a488f0fda566d
Daniel Imfeld <daniel@imfeld.dev>
2021-03-27 04:40:04 +0000

    Convert from ActixError

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,13 @@
-use actix_web::{http::StatusCode, HttpResponse};
+use actix_web::{dev::Body, http::StatusCode, HttpResponse};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
+    #[error("Authentication failure")]
+    AuthenticationError,
+    #[error("Unauthorized")]
+    AuthorizationError,
+
     #[error("SQL Error")]
     SqlError(#[from] sqlx::error::Error),
 
@@ -17,6 +22,25 @@ pub enum Error {
 
     #[error("timed out")]
     TimeoutError,
+
+    #[error("Actix error {:?}", body)]
+    ActixError {
+        status_code: StatusCode,
+        body: String,
+    },
+
+    #[error("Unspecified")]
+    Unspecified,
+}
+
+impl From<actix_web::Error> for Error {
+    fn from(e: actix_web::Error) -> Error {
+        let r = e.as_response_error();
+        Error::ActixError {
+            status_code: r.status_code(),
+            body: r.to_string(),
+        }
+    }
 }
 
 impl actix_web::error::ResponseError for Error {
@@ -25,27 +49,35 @@ impl actix_web::error::ResponseError for Error {
     }
 
     fn status_code(&self) -> StatusCode {
-        StatusCode::INTERNAL_SERVER_ERROR
-    }
-}
-
-impl sqlx::error::DatabaseError for Error {
-    fn message(&self) -> &str {
         match self {
-            Error::SqlError(sqlx::Error::Database(e)) => e.message(),
-            _ => "",
+            Error::AuthenticationError => StatusCode::UNAUTHORIZED,
+            Error::AuthorizationError => StatusCode::FORBIDDEN,
+            Error::ActixError { status_code, .. } => *status_code,
+            _ => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
-
-    fn as_error(&self) -> &(dyn std::error::Error + Send + Sync + 'static) {
-        self
-    }
-
-    fn as_error_mut(&mut self) -> &mut (dyn std::error::Error + Send + Sync + 'static) {
-        self
-    }
-
-    fn into_error(self: Box<Self>) -> Box<dyn std::error::Error + Send + Sync + 'static> {
-        self
-    }
 }
+
+// impl sqlx::error::DatabaseError for Error {
+//     fn message(&self) -> &str {
+//         match self {
+//             Error::SqlError(sqlx::Error::Database(e)) => e.message(),
+//             _ => "",
+//         }
+//     }
+//
+//     fn as_error(&self) -> &(dyn std::error::Error + Send + Sync + 'static) {
+//         match self {
+//             Error::ActixError(e) => &EMPTY_ERROR,
+//             _ => self,
+//         }
+//     }
+//
+//     fn as_error_mut(&mut self) -> &mut (dyn std::error::Error + Send + Sync + 'static) {
+//         self
+//     }
+//
+//     fn into_error(self: Box<Self>) -> Box<dyn std::error::Error + Send + Sync + 'static> {
+//         self
+//     }
+// }
