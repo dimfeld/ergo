3e84b828e02d2c51434fdcb3d6840235155fa0d6
Daniel Imfeld <daniel@imfeld.dev>
2021-04-07 05:24:55 +0000

    Fill in postgres side of drain

--- src/queues/postgres_drain.rs
+++ src/queues/postgres_drain.rs
@@ -23,8 +23,8 @@ pub struct QueueStageDrainConfig<'a> {
 /// This implements the drain stage of a transactionally-staged job drain, as described
 /// at https://brandur.org/job-drain.
 pub struct QueueStageDrain {
-    close: oneshot::Sender<()>,
-    join_handle: tokio::task::JoinHandle<()>,
+    close: Option<oneshot::Sender<()>>,
+    join_handle: Option<tokio::task::JoinHandle<()>>,
 }
 
 impl QueueStageDrain {
@@ -47,73 +47,118 @@ impl QueueStageDrain {
 
         let (close_tx, close_rx) = tokio::sync::oneshot::channel::<()>();
 
-        let join_handle = tokio::spawn(move || {
-            drain.queue_task(
-                config.db_pool,
-                db_select_query,
-                db_delete_query,
-                config.jetstream_queue,
-                config.nats_connection,
-                close_rx,
-                config.shutdown,
-            )
-        });
+        let QueueStageDrainConfig {
+            db_pool,
+            queue,
+            shutdown,
+            ..
+        } = config;
+
+        let join_handle = tokio::spawn(drain_task(
+            db_pool,
+            db_select_query,
+            db_delete_query,
+            queue,
+            close_rx,
+            shutdown,
+        ));
 
         Ok(QueueStageDrain {
-            close: close_tx,
-            join_handle,
+            close: Some(close_tx),
+            join_handle: Some(join_handle),
         })
     }
 
-    pub async fn close(&mut self) -> Result<(), Error> {
-        let QueueStageDrain { close, join_handle } = self;
-        close.send(()).ok();
-        join_handle.await
+    // Close without consuming the object. Needed for drop implementation.
+    fn close_internal(&mut self) -> Option<JoinHandle<()>> {
+        if let Some(close) = self.close.take() {
+            close.send(()).ok();
+        }
+
+        self.join_handle.take()
+    }
+
+    pub fn close(mut self) -> JoinHandle<()> {
+        self.close_internal().unwrap()
     }
 }
 
 impl Drop for QueueStageDrain {
     fn drop(&mut self) {
-        self.close()
+        self.close_internal();
     }
 }
 
 async fn drain_task(
-    db_pool: VaultPostgresPool,
+    db_pool: PostgresPool,
     db_select_query: String,
     db_delete_query: String,
-    jetstream_queue: String,
-    nats_connection: nats::Connection,
+    queue: Queue,
     close: oneshot::Receiver<()>,
-    shutdown: GracefulShutdownConsumer,
+    mut shutdown: GracefulShutdownConsumer,
 ) {
+    tokio::pin!(close);
+    let shutdown_waiter = shutdown.wait_for_shutdown();
+    tokio::pin!(shutdown_waiter);
+
     loop {
+        let db_select_query = db_select_query.clone();
+        let db_delete_query = db_delete_query.clone();
+
         // TODO no unwrap
-        let conn = db_pool.acquire().await.unwrap();
-        let found_some = conn.transaction(|tx| {
-            let db_select_query = &db_select_query;
-            let db_delete_query = &db_delete_query;
+        let mut conn = db_pool.acquire().await.unwrap();
+        let found_some = conn
+            .transaction(move |tx| {
+                Box::pin(async move {
+                    let lock_result = sqlx::query("SELECT pg_try_advisory_xact_lock(7893478934);")
+                        .fetch_one(&mut *tx)
+                        .await?;
+                    let acquired_lock: bool = lock_result.get(0);
+                    if acquired_lock == false {
+                        // Something else has the lock, so just exit and try again after a sleep.
+                        return Ok(false);
+                    }
 
-            Box::pin(async move {
-                let rows = sqlx::query(db_select_query.as_str()).fetch_all(tx).await?;
+                    let rows = sqlx::query(&db_select_query).fetch_all(&mut *tx).await?;
 
-                if rows.is_empty() {
-                    return Ok(false);
-                }
+                    if rows.is_empty() {
+                        return Ok(false);
+                    }
 
-                for row in rows {
-                    let id = row.get(1);
-                    let value = row.get(2);
-                }
+                    let queue_items = rows
+                        .into_iter()
+                        .map(|row| {
+                            (
+                                row.get::<String, usize>(0),
+                                row.get::<serde_json::Value, usize>(1),
+                            )
+                        })
+                        .collect::<Vec<_>>();
 
-                return Ok(true);
+                    // TODO enqueue items
+
+                    sqlx::query(&db_delete_query).execute(&mut *tx).await?;
+
+                    return Ok::<bool, Error>(true);
+                })
             })
-        });
+            .await;
+        drop(conn);
+
+        match found_some {
+            Ok(true) => {
+                continue;
+            }
+            Ok(false) => {}
+            Err(e) => {
+                event!(Level::ERROR, error=?e, "Error draining job queue");
+            }
+        };
 
         tokio::select! {
             _ = tokio::time::sleep(Duration::from_millis(100)) => continue,
-            _ = shutdown.wait_for_shutdown() => break,
-            _ = close => break,
+            _ = &mut shutdown_waiter => break,
+            _ = &mut close => break,
         }
     }
 }
