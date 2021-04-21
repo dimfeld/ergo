60ec08219d8c4d6d308ba4e5284a6a36c0d16e7a
Daniel Imfeld <daniel@imfeld.dev>
2021-04-21 02:02:32 +0000

    Update for new actions model
    This is a four-layer model:
      - Executors define the raw operations, such as "do an HTTP Request"
      - Actions define abstractions over executors, such as "call a Slack
        Incoming Webhook".
      - Task actions link tasks to actions, and can contain values to
        send to the actions.
      - State machines trigger task actions, and can provide their own
        values to help fill in the action template.
    
    Task actions can also reference accounts, for things that require
    API keys.

--- migrations/20210321000136_tasks_events_actions.down.sql
+++ migrations/20210321000136_tasks_events_actions.down.sql
@@ -2,14 +2,18 @@ BEGIN;
 
 DROP TABLE task_triggers_log;
 DROP TABLE task_triggers;
+DROP TABLE task_actions;
 DROP TABLE actions_log;
 DROP TABLE tasks;
+DROP TABLE allowed_action_account_types;
 DROP TABLE actions;
 DROP TYPE action_status;
-DROP TYPE action_executor;
 DROP TABLE action_categories;
+DROP TABLE executors;
 DROP TABLE inputs_log;
 DROP TABLE inputs;
 DROP TABLE input_categories;
+DROP TABLE accounts;
+DROP TABLE account_types;
 
 COMMIT;
