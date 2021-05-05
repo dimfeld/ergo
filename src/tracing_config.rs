265221ee6ce7138e460dfc39a26f9cafcd781df4
Daniel Imfeld <daniel@imfeld.dev>
2021-05-05 08:14:40 +0000

    Enable pretty logging

--- src/tracing_config.rs
+++ src/tracing_config.rs
@@ -1,17 +1,25 @@
 use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{fmt::time::ChronoUtc, layer::SubscriberExt, EnvFilter, Registry};
 
 pub fn configure(name: impl Into<String>) {
     LogTracer::init().expect("Failed to create logger");
 
     let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
-    let formatter = BunyanFormattingLayer::new(name.into(), std::io::stdout);
 
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatter);
-    set_global_default(subscriber).expect("Setting subscriber");
+    let subscriber = Registry::default().with(env_filter);
+
+    if envoption::with_default("LOG_PRETTY", false).unwrap() {
+        let formatter = tracing_subscriber::fmt::Layer::new().pretty();
+        let subscriber = subscriber.with(formatter);
+
+        set_global_default(subscriber).expect("Setting subscriber");
+    } else {
+        let formatter = tracing_subscriber::fmt::Layer::new()
+            .json()
+            .with_timer(ChronoUtc::rfc3339());
+        let subscriber = subscriber.with(formatter);
+
+        set_global_default(subscriber).expect("Setting subscriber");
+    }
 }
