c6e4a0335a3b2cdc13b3123e3cf919b6e18d6b38
Daniel Imfeld <daniel@imfeld.dev>
2021-05-10 07:36:49 +0000

    Switch back to tracing_bunyan_formatter

--- src/tracing_config.rs
+++ src/tracing_config.rs
@@ -1,25 +1,17 @@
 use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
-use tracing_subscriber::{fmt::time::ChronoUtc, layer::SubscriberExt, EnvFilter, Registry};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 pub fn configure(name: impl Into<String>) {
     LogTracer::init().expect("Failed to create logger");
 
     let env_filter = EnvFilter::try_from_env("LOG").unwrap_or(EnvFilter::new("info"));
 
-    let subscriber = Registry::default().with(env_filter);
-
-    if envoption::with_default("LOG_PRETTY", false).unwrap() {
-        let formatter = tracing_subscriber::fmt::Layer::new().pretty();
-        let subscriber = subscriber.with(formatter);
-
-        set_global_default(subscriber).expect("Setting subscriber");
-    } else {
-        let formatter = tracing_subscriber::fmt::Layer::new()
-            .json()
-            .with_timer(ChronoUtc::rfc3339());
-        let subscriber = subscriber.with(formatter);
-
-        set_global_default(subscriber).expect("Setting subscriber");
-    }
+    let formatting_layer = BunyanFormattingLayer::new(name.into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subscriber).expect("Setting subscriber");
 }
