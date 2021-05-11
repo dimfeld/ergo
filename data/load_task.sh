548168f972ec15ffa9384bfbeff9440c2b21fcc1
Daniel Imfeld <daniel@imfeld.dev>
2021-05-11 02:42:55 +0000

    add youtube-dl task

---
+++ data/load_task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+[ -f ../.env ] && source ../.env
+
+for file in "$@"; do
+  echo $file
+  env-template "$file" | http POST ${HOST:-http://localhost:6543}/api/tasks\?api_key=${API_KEY}
+done
