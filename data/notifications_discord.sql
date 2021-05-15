f603b2f34aaa7345ebd93e99a6221b8f520960ad
Daniel Imfeld <daniel@imfeld.dev>
2021-05-15 03:01:16 +0000

    Add SQL file for Discord notification configuration

---
+++ data/notifications_discord.sql
@@ -0,0 +1,13 @@
+INSERT INTO notify_endpoints
+  (notify_endpoint_id, org_id, service, destination)
+  VALUES
+  ({{BASE_NOTIFY_ENDPOINT_ID}}01, '{{ORG_ID}}', 'discord_incoming_webhook', '{{DISCORD_WEBHOOK_URL}}');
+
+INSERT INTO notify_listeners
+  (notify_listener_id, org_id, notify_endpoint_id, object_id, event)
+  VALUES
+  ({{BASE_NOTIFY_LISTENER_ID}}001, '{{ORG_ID}}', {{BASE_NOTIFY_ENDPOINT_ID}}01, 1, 'input_processed'),
+  ({{BASE_NOTIFY_LISTENER_ID}}002, '{{ORG_ID}}', {{BASE_NOTIFY_ENDPOINT_ID}}01, 1, 'action_started'),
+  ({{BASE_NOTIFY_LISTENER_ID}}003, '{{ORG_ID}}', {{BASE_NOTIFY_ENDPOINT_ID}}01, 1, 'action_success'),
+  ({{BASE_NOTIFY_LISTENER_ID}}004, '{{ORG_ID}}', {{BASE_NOTIFY_ENDPOINT_ID}}01, 1, 'action_error');
+  
