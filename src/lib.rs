d8cc2909cb867e9baedf2be2709e1b57371837d4
Daniel Imfeld <daniel@imfeld.dev>
2021-05-25 07:31:50 +0000

    Split lib/bin to support integration tests

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![allow(dead_code, unused_imports, unused_variables)] // Remove this once the basic application is up and working
+pub mod auth;
+pub mod backend_data;
+pub mod cmd;
+pub mod database;
+pub mod error;
+pub mod graceful_shutdown;
+pub mod notifications;
+pub mod queues;
+pub mod scripting;
+pub mod service_config;
+pub mod status_server;
+pub mod tasks;
+pub mod tracing_config;
+pub mod vault;
+pub mod web_app_server;
