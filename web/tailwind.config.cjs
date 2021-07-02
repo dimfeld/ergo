3c6823f9d0d83b221d54ecd6d6e1bfadff9bfb50
Daniel Imfeld <daniel@imfeld.dev>
2021-07-02 18:43:50 +0000

    add Inter font

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 const config = {
   mode: 'jit',
   purge: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
   darkMode: 'class',
