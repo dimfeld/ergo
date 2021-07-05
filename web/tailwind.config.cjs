37040751651775dc50686067a88f34305b9bfc5e
Daniel Imfeld <daniel@imfeld.dev>
2021-07-05 01:03:34 +0000

    Add orange as primary color (for now)

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,10 +1,14 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 const config = {
   mode: 'jit',
   purge: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      colors: {
+        primary: colors.orange,
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
