a6abbb1d33a6ddb514254968689134e63c6d2bb9
Daniel Imfeld <daniel@imfeld.dev>
2021-07-06 19:19:15 +0000

    Add store for header text

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,19 +1,26 @@
 <script lang="typescript">
   import '../app.css';
   import { page } from '$app/stores';
+  import { setContext } from 'svelte';
+  import { writable } from 'svelte/store';
   import { createDarkStore, cssDarkModePreference } from '../styles';
   import Nav from './_Nav.svelte';
 
   let darkModeStore = createDarkStore();
   $: darkMode = $darkModeStore ?? cssDarkModePreference();
   $: section = $page.path.split('/')[1];
+
+  let headerTextStore = writable('');
+  setContext('headerText', headerTextStore);
 </script>
 
-<div id="top" class:dark={darkMode} class="min-h-screen">
+<div id="top" class:dark={darkMode} class="min-h-screen overflow-y-auto overflow-x-hidden">
   <Nav {section} />
   <header class="bg-white dark:bg-black shadow-sm">
     <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-lg leading-6 font-semibold text-gray-900 dark:text-gray-100">Dashboard</h1>
+      <h1 class="text-lg leading-6 font-semibold text-gray-900 dark:text-gray-100">
+        {$headerTextStore}
+      </h1>
     </div>
   </header>
   <main>
