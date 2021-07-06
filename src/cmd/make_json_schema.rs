5ca4300abb3bc5458282460af26bb25b7a844e16
Daniel Imfeld <daniel@imfeld.dev>
2021-07-06 19:53:49 +0000

    Generate JSON schemas

--- src/cmd/make_json_schema.rs
+++ src/cmd/make_json_schema.rs
@@ -1,5 +1,15 @@
 use crate::tasks::{
-    actions::handlers::ActionPayload, handlers::TaskInput, inputs::handlers::InputPayload,
+    actions::{
+        execute::ScriptOrTemplate,
+        handlers::ActionPayload,
+        template::{TemplateField, TemplateFieldFormat},
+    },
+    handlers::TaskInput,
+    inputs::{handlers::InputPayload, Input},
+    state_machine::{
+        ActionInvokeDef, ActionInvokeDefDataField, ActionPayloadBuilder, EventHandler,
+        StateDefinition, StateMachine, StateMachineData, TransitionCondition, TransitionTarget,
+    },
 };
 
 use schemars::{schema::RootSchema, schema_for};
@@ -15,13 +25,61 @@ fn write(name: &str, schema: &RootSchema) -> std::io::Result<()> {
 }
 
 pub fn main() -> crate::error::Result<()> {
-    let task_input = schema_for!(TaskInput);
-    write("task_input", &task_input)?;
+    let dirs = std::env::current_dir().unwrap().join("schemas");
+    let e = std::fs::DirBuilder::new().create(&dirs);
+    if let Err(e) = e {
+        if e.kind() != std::io::ErrorKind::AlreadyExists {
+            return Err(e.into());
+        }
+    }
 
-    let action_payload = schema_for!(ActionPayload);
-    write("action_payload", &action_payload)?;
-    let input_payload = schema_for!(InputPayload);
-    write("input_payload", &input_payload)?;
+    let schema = schema_for!(TaskInput);
+    write("task_input", &schema)?;
+
+    let schema = schema_for!(TemplateField);
+    write("template_field", &schema)?;
+
+    let schema = schema_for!(TemplateFieldFormat);
+    write("template_field_format", &schema)?;
+
+    let schema = schema_for!(ActionPayload);
+    write("action_payload", &schema)?;
+
+    let schema = schema_for!(ActionPayloadBuilder);
+    write("action_payload_builder", &schema)?;
+
+    let schema = schema_for!(ActionInvokeDef);
+    write("action_invoke_def", &schema)?;
+
+    let schema = schema_for!(ActionInvokeDefDataField);
+    write("action_invoke_def_data_field", &schema)?;
+
+    let schema = schema_for!(ScriptOrTemplate);
+    write("script_or_template", &schema)?;
+
+    let schema = schema_for!(StateMachineData);
+    write("state_machine_data", &schema)?;
+
+    let schema = schema_for!(StateDefinition);
+    write("state_definition", &schema)?;
+
+    let schema = schema_for!(EventHandler);
+    write("event_handler", &schema)?;
+
+    let schema = schema_for!(TransitionTarget);
+    write("transition_target", &schema)?;
+
+    let schema = schema_for!(TransitionCondition);
+    write("transition_condition", &schema)?;
+
+    let schema = schema_for!(StateMachine);
+    write("state_machine", &schema)?;
+
+    let schema = schema_for!(Input);
+    write("input", &schema)?;
+
+    let schema = schema_for!(InputPayload);
+    write("input_payload", &schema)?;
 
     Ok(())
 }
