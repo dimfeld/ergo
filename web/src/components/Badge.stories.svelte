b7d9a43cf2fe613cce12e1f3e4556ba5e2002868
Daniel Imfeld <daniel@imfeld.dev>
2021-07-09 19:54:42 +0000

    Badge component

---
+++ web/src/components/Badge.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import Badge from './Badge.svelte';
+</script>
+
+<Meta title="Primitives/Badge" component={Badge} />
+
+<Template>
+  {#each ['', 'dark bg-gray-900'] as className}
+    <div class="p-4 flex flex-row space-x-4 {className}">
+      <Badge style="plain">Plain</Badge>
+      <Badge style="accent">Accent</Badge>
+      <Badge style="success">Success</Badge>
+      <Badge style="error">Error</Badge>
+    </div>
+  {/each}
+</Template>
+
+<Story name="Text" />
---
+++ web/src/components/Badge.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let style: 'success' | 'error' | 'plain' | 'accent' = 'plain';
+
+  const styles = {
+    success: 'bg-green-100 text-green-800 dark:bg-green-700 dark:text-green-200',
+    error: 'bg-red-100 text-red-800 dark:bg-red-700 dark:text-red-200',
+    plain: 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-200',
+    accent: 'bg-primary-100 text-primary-800 dark:bg-primary-700 dark:text-primary-200',
+  };
+</script>
+
+<p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full {styles[style]}">
+  <slot />
+</p>
