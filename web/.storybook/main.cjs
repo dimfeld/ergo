03aed4b6af5b10cf3aaa2ccd6de0fd696d6b3189
Daniel Imfeld <daniel@imfeld.dev>
2021-07-18 01:08:30 +0000

    use webpack5 storybook builder

--- web/.storybook/main.cjs
+++ web/.storybook/main.cjs
@@ -1,30 +1,39 @@
 const preprocess = require('svelte-preprocess');
 const postcssConfig = require('../postcss.config.cjs');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-svelte-csf",
-    "storybook-tailwind-dark-mode",
+  core: {
+    builder: 'webpack5',
+  },
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-svelte-csf',
+    'storybook-tailwind-dark-mode',
     {
-     name: '@storybook/addon-postcss',
-     options: {
-       postcssLoaderOptions: {
-         implementation: require('postcss'),
-         postcssOptions: postcssConfig,
-       },
-     },
-   },
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+          postcssOptions: postcssConfig,
+        },
+      },
+    },
   ],
-  "svelteOptions": {
-    "preprocess": preprocess({
+  svelteOptions: {
+    preprocess: preprocess({
       postcss: postcssConfig,
       typescript: true,
-      sourceMap: true
-    })
-  }
-}
+      sourceMap: true,
+    }),
+  },
+  async webpackFinal(config) {
+    config.module.rules.push({
+      resolve: {
+        fullySpecified: false,
+        extensions: ['.js', '.ts', '.json', '.svelte'],
+      },
+    });
+    return config;
+  },
+};
