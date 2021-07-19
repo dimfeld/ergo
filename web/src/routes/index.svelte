2d75682d2d080121e40edc2c790897d082a84021
Daniel Imfeld <daniel@imfeld.dev>
2021-07-19 02:20:32 +0000

    Show task list

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,19 +1,30 @@
 <script lang="ts">
   import type { Writable } from 'svelte/store';
-  import type { InputsLogEntry } from '../api_types';
+  import type { InputsLogEntry, TaskDescription } from '../api_types';
   import LogTimeline from '^/components/LogTimeline.svelte';
+  import Loading from '^/components/Loading.svelte';
+  import TaskRow from '^/components/TaskRow.svelte';
   import { useQuery } from '@sveltestack/svelte-query';
   import { getContext } from 'svelte';
   getContext<Writable<string>>('headerText').set('Dashboard');
 
   const recentLogs = useQuery<InputsLogEntry[]>('logs');
+  const tasks = useQuery<TaskDescription[]>('tasks');
 </script>
 
 <div class="flex">
-  <section class="flex-grow min-w-max">Task list here</section>
+  <section class="flex-grow min-w-max">
+    {#if $tasks.isLoading}
+      <Loading />
+    {:else}
+      {#each $tasks.data ?? [] as task (task.id)}
+        <TaskRow {task} />
+      {/each}
+    {/if}
+  </section>
   <section class="min-h-full ml-4">
     {#if $recentLogs.isLoading}
-      Loading...
+      <Loading />
     {:else}
       <LogTimeline entries={$recentLogs.data} />
     {/if}
