9f462c2367fd81b98a09f86555055a67f5478a0b
Daniel Imfeld <daniel@imfeld.dev>
2021-07-19 02:15:46 +0000

    sliding arrows loader

---
+++ web/src/components/Loading.stories.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  import Loading from './Loading.svelte';
+</script>
+
+<Meta title="Primitives/Loading" component={Loading} />
+
+<Template>
+  <div class="w-3/4 border border-gray-200 h-48">
+    <Loading />
+  </div>
+</Template>
+
+<Story id="loader_story" name="Loading" />
---
+++ web/src/components/Loading.svelte
@@ -0,0 +1,75 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  class="h-12 w-full overflow-hidden"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  preserveAspectRatio="true"
+>
+  <path
+    class="right-arrow"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M6 7h12m0 0l-4-4m4 4l-4 4m0"
+  />
+  <path
+    class="left-arrow"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M16 16H4m0 0l4 4m-4-4l4-4"
+  />
+</svg>
+
+<style>
+  @keyframes goleft {
+    from {
+      transform: translateX(1000%);
+    }
+
+    60% {
+      transform: translateX(0%);
+      opacity: 1;
+    }
+
+    90% {
+      transform: translateX(0%);
+      opacity: 1;
+    }
+
+    to {
+      transform: translateX(0%);
+      opacity: 0;
+    }
+  }
+
+  @keyframes goright {
+    from {
+      transform: translateX(-1000%);
+    }
+
+    60% {
+      transform: translateX(0%);
+      opacity: 1;
+    }
+
+    90% {
+      transform: translateX(0%);
+      opacity: 1;
+    }
+
+    to {
+      transform: translateX(0%);
+      opacity: 0;
+    }
+  }
+
+  .left-arrow {
+    animation: 2s ease-out infinite goleft;
+  }
+
+  .right-arrow {
+    animation: 2s ease-out infinite goright;
+  }
+</style>
