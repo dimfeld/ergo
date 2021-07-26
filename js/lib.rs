27bcc9d6661f4a379e2c34eb29340efa67382568
Daniel Imfeld <daniel@imfeld.dev>
2021-07-26 20:49:18 +0000

    Inject values and evaluate expressions in V8

--- js/lib.rs
+++ js/lib.rs
@@ -1,6 +1,9 @@
 use deno_core::{error::AnyError, JsRuntime, RuntimeOptions};
-use rusty_v8::{Global, Value};
+use rusty_v8 as v8;
+use rusty_v8::{Global, ObjectTemplate, Value};
+use serde::{Deserialize, Serialize};
 use serde_v8::{from_v8, to_v8};
+use v8::Handle;
 
 pub struct Snapshot(Box<[u8]>);
 
@@ -52,6 +55,30 @@ impl Runtime {
         let value = local.number_value(&mut scope).unwrap();
         Ok(value)
     }
+
+    pub fn run_boolean_expression<T: Serialize>(
+        &mut self,
+        value: &T,
+        expression: &str,
+    ) -> Result<bool, AnyError> {
+        self.insert_global_object("value", value);
+
+        let result = self
+            .runtime
+            .execute_script("boolean expression", expression)?;
+        let local = result.get(&mut self.runtime.v8_isolate());
+        let mut scope = self.runtime.handle_scope();
+        Ok(local.boolean_value(&mut scope))
+    }
+
+    pub fn insert_global_object<T: Serialize>(&mut self, key: &str, value: &T) {
+        let mut scope = self.runtime.handle_scope();
+        let jskey = v8::String::new(&mut scope, key).unwrap();
+        let value = to_v8(&mut scope, value).unwrap();
+
+        let global = scope.get_current_context().global(&mut scope);
+        global.set(&mut scope, jskey.into(), value);
+    }
 }
 
 #[cfg(test)]
@@ -63,5 +90,15 @@ mod tests {
         let mut runtime = Runtime::new();
         let value = runtime.run_expression("test_add", "3 + 4").expect("Ok");
         assert_eq!(value, 7.0);
+    }
+
+    #[test]
+    fn boolean_expression() {
+        let mut runtime = Runtime::new();
+        let result = runtime.run_boolean_expression(&5, "value === 5").unwrap();
+        assert_eq!(result, true, "value === 5 where value is 5");
+
+        let result = runtime.run_boolean_expression(&1, "value > 2").unwrap();
+        assert_eq!(result, false, "value > 2 where value is 1");
     }
 }
