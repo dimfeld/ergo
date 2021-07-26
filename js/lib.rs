49d9a9414e547dc04603f69f8fa896a48b858631
Daniel Imfeld <daniel@imfeld.dev>
2021-07-26 00:37:20 +0000

    Really simple expression evaluation

--- js/lib.rs
+++ js/lib.rs
@@ -1,4 +1,6 @@
-use deno_core::{JsRuntime, RuntimeOptions};
+use deno_core::{error::AnyError, JsRuntime, RuntimeOptions};
+use rusty_v8::{Global, Value};
+use serde_v8::{from_v8, to_v8};
 
 pub struct Snapshot(Box<[u8]>);
 
@@ -39,5 +41,27 @@ impl Runtime {
     pub fn make_snapshot(&mut self) -> Snapshot {
         let snapshot = self.runtime.snapshot();
         Snapshot(snapshot.as_ref().to_vec().into_boxed_slice())
+    }
+
+    /** Run an expression and return the value */
+    pub fn run_expression(&mut self, name: &str, text: &str) -> Result<f64, AnyError> {
+        let result = self.runtime.execute_script(name, text)?;
+        let mut isolate = self.runtime.v8_isolate();
+        let local = result.get(&mut isolate);
+        let mut scope = self.runtime.handle_scope();
+        let value = local.number_value(&mut scope).unwrap();
+        Ok(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_expression() {
+        let mut runtime = Runtime::new();
+        let value = runtime.run_expression("test_add", "3 + 4").expect("Ok");
+        assert_eq!(value, 7.0);
     }
 }
