f9211db534925d0dcb4280cacdce86bcc2baf8cb
Daniel Imfeld <daniel@imfeld.dev>
2021-08-04 02:55:07 +0000

    Save event args and function name
    to verify determinstic calling on later retrieval

--- js/serialized_execution.rs
+++ js/serialized_execution.rs
@@ -1,8 +1,10 @@
 use chrono::{DateTime, Utc};
 use rusty_v8 as v8;
 use serde::{Deserialize, Serialize};
-use serde_v8::to_v8;
+use serde_v8::{from_v8, to_v8};
 use v8::{Exception, MapFnTo};
+
+use crate::raw_serde;
 
 lazy_static::lazy_static! {
     pub static ref EXTERNAL_REFERENCES: v8::ExternalReferences = v8::ExternalReferences::new(&[
@@ -19,6 +21,10 @@ lazy_static::lazy_static! {
 pub struct SerializedEvent {
     /// The wall time when the event completes.
     wall_time: chrono::DateTime<Utc>,
+    /// Serialized version of the arguments used to generate this result, which will
+    /// be compared on retrieval to ensure that the script is running deterministically.
+    args: Vec<u8>,
+    fn_name: String,
     result: Vec<u8>,
 }
 
@@ -75,6 +81,7 @@ pub fn install(runtime: &mut crate::Runtime, history: SerializedState) -> () {
     set_func(scope, ser_obj, "saveResult", save_result);
     set_func(scope, ser_obj, "getResult", get_result);
     set_func(scope, ser_obj, "wallTime", wall_time);
+    set_func(scope, ser_obj, "exit", exit);
 
     let global = scope.get_current_context().global(scope);
     global.set(scope, jskey.into(), ser_obj.into());
@@ -104,15 +111,64 @@ fn set_func(
     object.set(scope, key.into(), v8_func.into());
 }
 
+/// Exit the script early.
+fn exit(scope: &mut v8::HandleScope, _args: v8::FunctionCallbackArguments, _rv: v8::ReturnValue) {
+    scope.terminate_execution();
+}
+
+macro_rules! convert_arg {
+    ($scope: expr, $value: expr, $err: expr) => {
+        match from_v8($scope, $value) {
+            Ok(value) => value,
+            Err(_) => {
+                throw_error($scope, $err);
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! get_event_state {
+    ($scope: expr) => {
+        match $scope.get_slot_mut::<EventTracker>() {
+            Some(e) => e,
+            None => {
+                throw_error($scope, "Serialized execution not enabled");
+                return;
+            }
+        }
+    };
+}
+
 fn save_result(
     scope: &mut v8::HandleScope,
     args: v8::FunctionCallbackArguments,
-    mut rv: v8::ReturnValue,
+    _rv: v8::ReturnValue,
 ) {
-    todo!();
+    let fn_name: String = convert_arg!(
+        scope,
+        args.get(0),
+        "First argument must be the name of the function"
+    );
+
+    let fn_args = raw_serde::serialize(scope, args.get(1)).unwrap();
+    let result = raw_serde::serialize(scope, args.get(2)).unwrap();
+    let events = get_event_state!(scope);
+
+    let new_wall_time = Utc::now();
+    events.wall_time = new_wall_time;
+    events.saved_results.push(SerializedEvent {
+        wall_time: new_wall_time,
+        fn_name,
+        args: fn_args,
+        result,
+    });
 }
 
-/// Get the next result, if any.
+/// Get the next result, if any. This is called with the arguments object,
+/// which is checked against the saved events object to ensure that the functions
+/// are being called with the same sequence and arguments as what was saved.
+/// From Javascript: getResult(argumentsObject);
 fn get_result(
     scope: &mut v8::HandleScope,
     args: v8::FunctionCallbackArguments,
@@ -124,20 +180,17 @@ fn get_result(
 /// Get the serialized wall time.
 fn wall_time(
     scope: &mut v8::HandleScope,
-    args: v8::FunctionCallbackArguments,
+    _args: v8::FunctionCallbackArguments,
     mut rv: v8::ReturnValue,
 ) {
-    let events = match scope.get_slot::<EventTracker>() {
-        Some(e) => e,
-        None => {
-            let msg = v8::String::new(scope, "Serialized execution not enabled").unwrap();
-            let exc = Exception::error(scope, msg);
-            scope.throw_exception(exc);
-            return;
-        }
-    };
-
+    let events = get_event_state!(scope);
     let time_ms = events.wall_time.timestamp_millis();
     let time_v8 = to_v8(scope, &time_ms).unwrap();
     rv.set(time_v8);
+}
+
+fn throw_error(scope: &mut v8::HandleScope, err: &'static str) {
+    let msg = v8::String::new(scope, err).unwrap();
+    let exc = Exception::error(scope, msg);
+    scope.throw_exception(exc);
 }
