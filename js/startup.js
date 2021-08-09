a503b8aff59998870f2aee9948be5aa7ff9622db
Daniel Imfeld <daniel@imfeld.dev>
2021-08-09 19:08:12 +0000

    Add more stuff from __bootstrap to get `fetch` working

--- js/startup.js
+++ js/startup.js
@@ -3,8 +3,10 @@
 // This isn't the right way to do it, but it's enough to get things sort of working for now.
 Object.assign(globalThis,
   globalThis.__bootstrap.primordials,
-  globalThis.__bootstrap.url,
   globalThis.__bootstrap.crypto,
   globalThis.__bootstrap.fetch,
+  globalThis.__bootstrap.file,
+  globalThis.__bootstrap.streams,
+  globalThis.__bootstrap.url,
 );
 globalThis.colors = globalThis.__bootstrap.colors;
