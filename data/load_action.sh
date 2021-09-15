4433f0fe31ab9a35d050200960fba08858ebb058
Daniel Imfeld <daniel@imfeld.dev>
2021-09-15 08:12:37 +0000

    Let data files be written in JSON5

--- data/load_action.sh
+++ data/load_action.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 for file in "$@"; do
   echo $file
-  object=$(env-template "$file")
+  object=$(env-template "$file" | json5)
   id=$(jq -r .action_id <<< "$object")
   http PUT ${HOST:-http://localhost:6543}/api/actions/$id\?api_key=${API_KEY} <<<"$object"
 done
--- data/load_task.sh
+++ data/load_task.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 
 for file in "$@"; do
   echo $file
-  object=$(env-template "$file")
+  object=$(env-template "$file" | json5)
   env-template "$file" | http POST ${HOST:-http://localhost:6543}/api/tasks\?api_key=${API_KEY}
 done
