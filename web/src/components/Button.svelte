4ac9d0caf21c4ed003bac0280c4f8cb2fb6ab9bd
Daniel Imfeld <daniel@imfeld.dev>
2021-09-20 08:15:04 +0000

    Button variants

--- web/src/components/Button.svelte
+++ web/src/components/Button.svelte
@@ -2,8 +2,24 @@
   export let element: HTMLButtonElement | undefined = undefined;
   export let disabled = false;
 
+  export let size: keyof typeof sizes = 'md';
+  export let style: keyof typeof styles = 'normal';
+
   let classNames = '';
   export { classNames as class };
+
+  const sizes = {
+    xs: 'px-2.5 py-1.5 text-xs rounded',
+    sm: 'px-3 py-2 leading-4 text-sm rounded-md',
+    md: 'px-4 py-2 text-sm rounded-md',
+  };
+
+  const styles = {
+    primary: 'border-accent-300 dark:border-accent-700',
+    normal: 'border-gray-300 dark:borde-gray-700',
+  };
+
+  $: dynamicClasses = `${sizes[size]} ${styles[style]} ${classNames}`;
 </script>
 
 <button
@@ -11,7 +27,7 @@
   type="button"
   {disabled}
   on:click
-  class="inline-flex justify-center rounded-md border border-accent-300 dark:border-accent-700 shadow-sm px-4 py-2 bg-white dark:bg-black text-sm font-medium text-gray-700 disabled:text-gray-400 dark:text-gray-300 dark:disabled:text-gray-400 hover:bg-accent-50 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900 focus:ring-accent-500 dark:focus:ring-accent-700 {classNames}"
+  class="inline-flex justify-center border shadow-sm bg-white dark:bg-black text-sm font-medium text-gray-700 disabled:text-gray-400 dark:text-gray-300 dark:disabled:text-gray-400 hover:bg-accent-50 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 dark:focus:ring-offset-gray-900 focus:ring-accent-500 dark:focus:ring-accent-700 {dynamicClasses}"
   class:cursor-not-allowed={disabled}
   ><slot />
 </button>
