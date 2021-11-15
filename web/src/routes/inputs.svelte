0623e3156be8dfa8bc21e46ef47dbee22e539ad2
Daniel Imfeld <daniel@imfeld.dev>
2021-11-15 20:47:15 +0800

    simple readonly inputs view

---
+++ web/src/routes/inputs.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { baseData } from '$lib/data';
+  import { getHeaderTextStore } from '$lib/header';
+  const { inputs } = baseData();
+  getHeaderTextStore().set(['Inputs']);
+</script>
+
+<ul class="space-y-4">
+  {#each Array.from($inputs.values()) as input (input.input_id)}
+    <li>
+      {input.name}
+      <ul class="ml-4">
+        {#each Object.entries(input.payload_schema.properties) as [field, fieldType] (field)}
+          <li>
+            <span class="text-gray-800 dark:text-gray-200 font-medium">{field}</span>: {fieldType.type}
+            {#if fieldType.format}
+              in {fieldType.format} format{/if}
+          </li>
+        {/each}
+      </ul>
+    </li>
+  {/each}
+</ul>
