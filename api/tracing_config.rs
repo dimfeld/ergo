00adb40ab8014877e22a1592e0f8d6d57f5b6133
Daniel Imfeld <daniel@imfeld.dev>
2022-12-10 00:01:20 +0000

    Use tracing-tree formatter

--- api/tracing_config.rs
+++ api/tracing_config.rs
@@ -2,8 +2,12 @@ use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
+use tracing_tree::HierarchicalLayer;
 
-pub fn configure(name: impl Into<String>, sink: impl MakeWriter + Send + Sync + 'static) {
+pub fn configure<W>(name: impl Into<String>, sink: W)
+where
+    for<'writer> W: MakeWriter<'writer> + Send + Sync + 'static,
+{
     LogTracer::builder()
         .ignore_crate("rustls")
         .with_max_level(log::LevelFilter::Debug)
@@ -12,7 +16,11 @@ pub fn configure(name: impl Into<String>, sink: impl MakeWriter + Send + Sync + 
 
     let env_filter = EnvFilter::try_from_env("LOG").unwrap_or_else(|_| EnvFilter::new("info"));
 
-    let formatting_layer = BunyanFormattingLayer::new(name.into(), sink);
+    // let formatting_layer = BunyanFormattingLayer::new(name.into(), sink);
+    let formatting_layer = HierarchicalLayer::new(2)
+        .with_bracketed_fields(true)
+        .with_targets(true)
+        .with_writer(sink);
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
--- test/lib.rs
+++ test/lib.rs
@@ -5,8 +5,12 @@ use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{fmt::MakeWriter, layer::SubscriberExt, EnvFilter, Registry};
+use tracing_tree::HierarchicalLayer;
 
-fn configure_tracing(name: impl Into<String>, sink: impl MakeWriter + Send + Sync + 'static) {
+fn configure_tracing<W>(name: impl Into<String>, sink: W)
+where
+    for<'writer> W: MakeWriter<'writer> + Send + Sync + 'static,
+{
     LogTracer::builder()
         .ignore_crate("rustls")
         .with_max_level(log::LevelFilter::Debug)
@@ -15,7 +19,11 @@ fn configure_tracing(name: impl Into<String>, sink: impl MakeWriter + Send + Syn
 
     let env_filter = EnvFilter::try_from_env("LOG").unwrap_or_else(|_| EnvFilter::new("info"));
 
-    let formatting_layer = BunyanFormattingLayer::new(name.into(), sink);
+    // let formatting_layer = BunyanFormattingLayer::new(name.into(), sink);
+    let formatting_layer = HierarchicalLayer::new(2)
+        .with_bracketed_fields(true)
+        .with_targets(true)
+        .with_writer(sink);
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
