e6730b70ef54a5ab16b5c21c1533c1b65ef78785
Daniel Imfeld <daniel@imfeld.dev>
2023-02-02 06:38:01 +0000

    Add more explciit drag handle to nodes

--- web/src/lib/editors/dataflow/DataFlowNode.svelte
+++ web/src/lib/editors/dataflow/DataFlowNode.svelte
@@ -25,6 +25,7 @@
   bind:position={node.meta.position}
   {selectMode}
   {selected}
+  minSize={{ x: 250, y: 150 }}
   on:mousemove
   on:mouseleave
   on:selectModeClick>
@@ -40,7 +41,7 @@
         class="absolute inset-0 border-transparent !bg-transparent px-2 py-0 text-xs font-medium text-accent-800 hover:border-gray-500 " />
     </div>
 
-    <div class="flex gap-4 pt-px text-xs text-accent-800">
+    <div class="flex gap-2 pt-px text-xs text-accent-800">
       <button
         type="button"
         class="no-drag whitespace-nowrap hover:text-accent-600"
@@ -50,11 +51,24 @@
 
       <button
         type="button"
-        class="no-drag whitespace-nowrap hover:text-accent-600"
+        class="no-drag whitespace-nowrap py-1 hover:text-accent-600"
         on:click={() => dispatch('forceRun')}>
-        RUN
+        <!-- heroicons play button -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-4 w-4">
+          <path
+            fill-rule="evenodd"
+            d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm6.39-2.908a.75.75 0 01.766.027l3.5 2.25a.75.75 0 010 1.262l-3.5 2.25A.75.75 0 018 12.25v-4.5a.75.75 0 01.39-.658z"
+            clip-rule="evenodd" />
+        </svg>
       </button>
     </div>
+
+    <!-- Drag handle -->
+    <div class="mx-2 h-[7px] w-8 flex-grow self-center border-y-2 border-accent-700/70" />
 
     <button
       type="button"
